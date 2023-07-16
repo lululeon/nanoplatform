9d9b130c94c59b2021ad94f86805671a3123ecb0
lululeon <thelululeon@gmail.com>
2023-07-16 05:39:33 -0400

    minor refac to align/correct st recipes. add basic rls policies

--- auth/pkg/authz.go
+++ auth/pkg/authz.go
@@ -6,6 +6,8 @@ import (
 	"github.com/supertokens/supertokens-golang/recipe/session"
 	"github.com/supertokens/supertokens-golang/recipe/thirdparty"
 	"github.com/supertokens/supertokens-golang/recipe/thirdparty/tpmodels"
+	"github.com/supertokens/supertokens-golang/recipe/thirdpartyemailpassword"
+	"github.com/supertokens/supertokens-golang/recipe/thirdpartyemailpassword/tpepmodels"
 	"github.com/supertokens/supertokens-golang/recipe/userroles"
 	"github.com/supertokens/supertokens-golang/supertokens"
 )
@@ -17,7 +19,7 @@ type RolePerm struct {
 
 func InitSupertokensAuth(supertokensServerUrl string) error {
 	log.Println("InitSupertokensAuth : >>> starting...")
-	apiBasePath := "/auth"
+	apiBasePath := "/api/auth"
 	websiteBasePath := "/auth"
 	err := supertokens.Init(supertokens.TypeInput{
 		Supertokens: &supertokens.ConnectionInfo{
@@ -27,38 +29,26 @@ func InitSupertokensAuth(supertokensServerUrl string) error {
 		},
 		AppInfo: supertokens.AppInfo{
 			AppName:         "Unbuilt",
-			APIDomain:       "http://localhost:5000",
+			APIDomain:       "http://localhost:3000",
 			WebsiteDomain:   "http://localhost:3000",
 			APIBasePath:     &apiBasePath,
 			WebsiteBasePath: &websiteBasePath,
 		},
 		RecipeList: []supertokens.Recipe{
-			thirdparty.Init(&tpmodels.TypeInput{
-				SignInAndUpFeature: tpmodels.TypeInputSignInAndUp{
-					Providers: []tpmodels.TypeProvider{
-						// We have provided you with development keys which you can use for testing.
-						// IMPORTANT: Please replace them with your own OAuth keys for production use.
-						thirdparty.Google(tpmodels.GoogleConfig{
-							ClientID:     "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
-							ClientSecret: "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",
-						}),
-						thirdparty.Github(tpmodels.GithubConfig{
-							ClientID:     "467101b197249757c71f",
-							ClientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
-						}),
-						// thirdparty.Apple(tpmodels.AppleConfig{
-						// 	ClientID: "4398792-io.supertokens.example.service",
-						// 	ClientSecret: tpmodels.AppleClientSecret{
-						// 		KeyId:      "7M48Y4RYDL",
-						// 		PrivateKey: "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
-						// 		TeamId:     "YWQCXGJRJL",
-						// 	},
-						// }),
-						// thirdparty.Facebook(tpmodels.FacebookConfig{
-						//    ClientID:     "FACEBOOK_CLIENT_ID",
-						//    ClientSecret: "FACEBOOK_CLIENT_SECRET",
-						// }),
-					}}}),
+			thirdpartyemailpassword.Init(&tpepmodels.TypeInput{
+				Providers: []tpmodels.TypeProvider{
+					// We have provided you with development keys which you can use for testing.
+					// IMPORTANT: Please replace them with your own OAuth keys for production use.
+					thirdparty.Google(tpmodels.GoogleConfig{
+						ClientID:     "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
+						ClientSecret: "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",
+					}),
+					thirdparty.Github(tpmodels.GithubConfig{
+						ClientID:     "467101b197249757c71f",
+						ClientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
+					}),
+				},
+			}),
 			session.Init(nil),   // initializes session features
 			userroles.Init(nil), // initialise userroles features
 		},
